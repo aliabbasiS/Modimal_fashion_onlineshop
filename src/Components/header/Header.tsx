b5954cc387eca36e3323913f2cc94b6e5d7de57a
Ali <ali.abbasi.1382.shiraz@gmail.com>
2024-07-28 18:24:59 +0330

    fc: install tailwind create main page and try to add header fix app.tsx and do first task

---
+++ src/Components/header/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header>
+       <div className=" w-full tracking-widest	 leading-3 text-center text-white py-2 text-sm  kale_ghaz ">Enjoy Free Shipping On All Orders</div>
+        <div className=" h-11 "> <img src="" alt="" /></div>
+      </header>
+    );
+  }
+  
+  export default Header;
